ddbe5fa6ef84691b938aefbf1f6a8d8f25640e16
Hady Hesham <zeusmastergm@gmail.com>
2023-07-13 08:19:09 -0700

    Hi C language

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -rpints a given phrase
+ * return : 0 if exited properly , non-zero otherwise
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building and multilingual puzzle");
+	return (0);
+}
