1c957b7af356cb56451ca3df7a2311c689b8848b
Hady Hesham <zeusmastergm@gmail.com>
2023-07-31 14:27:50 -0700

    hotfix

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,34 +1,24 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * _strstr - locate a substring
- * @haystack: string
- * @needle: string substring
- * Return: Pointer to beginning of substring, or NULL if not found.
+ * print_chessboard - print the chessboard, 2d array
+ * @a: 2d array of chars
  */
 
-char *_strstr(char *haystack, char *needle)
+void print_chessboard(char (*a)[8])
 {
-	int i, j, c;
+	int y, x;
 
-	i = 0;
-	c = 0;
-	while (haystack[i] != '\0')
+	y = 0;
+	while (y < 8)
 	{
-		j = 0;
-		while (needle[j + c] != '\0' && haystack[i + c] != '\0'
-		       && needle[j + c] == haystack[i + c])
+		x = 0;
+		while (x < 8)
 		{
-			if (haystack[i + c] != needle[j + c])
-				break;
-			c++;
+			_putchar(a[y][x]);
+			x++;
 		}
-		if (needle[j + c] == '\0')
-			return (&haystack[i]);
-		j++;
-		i++;
+		_putchar('\n');
+		y++;
 	}
-
-	return (NULL);
-
+}
